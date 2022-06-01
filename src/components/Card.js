affa6e1bdcc382ba4620878e81b095948e041dc5
David Xu <david.xu@duke.edu>
2022-06-01 17:11:53 -0400

    pushin

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,15 @@ import img4 from '../images/star.png'
 export default function Card() {
     return (
         <div className="card">
-            <img src = {img3} alt=""/>
-            <img src = {img4} alt=""/>
+            <img src = {img3} alt="" className="image--card"/>
+            <div className="card--inner">
+                <img src = {img4} alt="" className="star"/>
+                <span >5.0</span>
+                <span className="grey">(6) • </span>
+                <span className="grey">USA</span>
+            </div>
+            <p className="bottom">Life Lessons with Katie Zaferes</p>
+            <p className="bottom"> <span className="bold">From $136/person</span></p>
         </div>
     )
 }
