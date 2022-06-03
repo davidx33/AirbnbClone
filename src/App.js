086bfe2401ac5191687cdb40bb9449db69961bce
David Xu <david.xu@duke.edu>
2022-06-03 10:05:28 -0400

    made Joke.js, did some practice with props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "./components/Navbar.js"
 import Hero from './components/Hero.js';
 import Card from './components/Card.js';
+import Joke from './Joke.js';
 
 function App() {
   return (
@@ -9,8 +10,41 @@ function App() {
       <Navbar />
       <Hero />
       <Card />
+      <Joke setup = "How did the hacker escape the police?" 
+      punchline = "He just ransomware!"/>
+      <Joke setup = "Why don't pirates travel on mountain roads?"
+      punchline = "Scurvy"/>
+      <Joke setup = "Why do bees stay in the hive in the winter?"
+      punchline = "Swarm"/>
+      <Joke setup = "What's the best thing about Switzerland?"
+      punchline = "I don't know, but the flag is a big plus!"/>
     </div>
   );
 }
 
 export default App;
+
+
+
+/* Challenge:
+One LAST time in this course, set up a React app from scratch
+- Render an <App /> component
+    - App should be in its own file
+- App should render 4-5 <Joke /> components 
+  (Joke component defined in its own file too)
+    - Each Joke should receive a "setup" prop and a "punchline" prop
+      and render those however you'd like
+- Use your favorite 2-part jokes (setup & punchline), or check
+  jokes.md file for some examples.
+
+
+
+EXTRA CREDIT:
+Some jokes are only a punchline with no setup:
+
+E.g.: "It’s hard to explain puns to kleptomaniacs because 
+they always take things literally."
+
+If you don't pass in a "question" prop, how might you make it only 
+show the punchline?
+*/
---
+++ src/Joke.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Joke(props) {
+    return (
+        <div>
+            <h3>{props.setup}</h3>
+            <h3>{props.punchline}</h3>
+        </div>
+    )
+}
